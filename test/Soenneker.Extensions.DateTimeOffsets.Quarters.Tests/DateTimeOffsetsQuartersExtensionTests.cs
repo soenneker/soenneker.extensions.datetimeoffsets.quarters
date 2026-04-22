7e7bc000711ea7863f4e031835ae47ac90ba8bcb
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:53 -0500

    TUnit conversion

--- test/Soenneker.Extensions.DateTimeOffsets.Quarters.Tests/DateTimeOffsetsQuartersExtensionTests.cs
+++ test/Soenneker.Extensions.DateTimeOffsets.Quarters.Tests/DateTimeOffsetsQuartersExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.DateTimeOffsets.Quarters.Tests;
 
 public sealed class DateTimeOffsetsQuartersExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
